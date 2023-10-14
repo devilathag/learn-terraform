4b7bf10b120b4fa44f3a304b8ee3272816b94cb6
devilatha <latha.korm@gmail.com>
2023-10-14 12:25:15 +0530

    learning terraform

--- 01.resources/main.tf
+++ 01.resources/main.tf
@@ -1,16 +1,17 @@
-resource "aws_instance" "web" {
+resource "aws_instance" "frontend" {
    ami           = "ami-03265a0778a880afb"
    instance_type = "t3.micro"
+   vpc_security_group_ids = ["sg-0d4a3adf35216c3e8"]
 
    tags = {
-      Name = "helloworld"
+      Name = "frontend"
    }
 }
 
-resource "aws_route53_record" "www" {
+resource "aws_route53_record" "frontend" {
   zone_id = "Z0159581LHW2KQMKKCYV"
-  name    = "test.gdevopsb72.online"
+  name    = "frontend.gdevopsb72.online"
   type    = "A"
   ttl     = 30
-  records = [ aws_instance.web.private_ip ]
+  records = [ aws_instance.frontend.private_ip ]
 }
---
+++ expense.v1/main.tf
@@ -0,0 +1,53 @@
+resource "aws_instance" "frontend" {
+   ami           = "ami-03265a0778a880afb"
+   instance_type = "t3.micro"
+   vpc_security_group_ids = ["sg-0d4a3adf35216c3e8"]
+
+   tags = {
+      Name = "frontend"
+   }
+}
+
+resource "aws_route53_record" "frontend" {
+  zone_id = z
+  name    = "frontend.gdevopsb72.online"
+  type    = "A"
+  ttl     = 30
+  records = [ aws_instance.frontend.private_ip ]
+}
+
+resource "aws_instance" "backend" {
+   ami           = "ami-03265a0778a880afb"
+   instance_type = "t3.micro"
+   vpc_security_group_ids = ["sg-0d4a3adf35216c3e8"]
+
+   tags = {
+      Name = "backend"
+   }
+}
+
+resource "aws_route53_record" "backend" {
+  zone_id = z
+  name    = "backend.gdevopsb72.online"
+  type    = "A"
+  ttl     = 30
+  records = [ aws_instance.backend.private_ip ]
+}
+
+resource "aws_instance" "mysql" {
+   ami           = "ami-03265a0778a880afb"
+   instance_type = "t3.micro"
+   vpc_security_group_ids = ["sg-0d4a3adf35216c3e8"]
+
+   tags = {
+      Name = "mysql"
+   }
+}
+
+resource "aws_route53_record" "mysql" {
+  zone_id = z
+  name    = "mysql.gdevopsb72.online"
+  type    = "A"
+  ttl     = 30
+  records = [ aws_instance.mysql.private_ip ]
+}
