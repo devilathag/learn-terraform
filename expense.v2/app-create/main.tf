6f90f57ae9181f4d2fb48a564d6fd25d9d3fd3c2
devilatha <latha.korm@gmail.com>
2023-10-20 12:14:22 +0530

    learning terraform

--- expense.v2/app-create/main.tf
+++ expense.v2/app-create/main.tf
@@ -4,26 +4,26 @@ resource "aws_instance" "instance" {
    vpc_security_group_ids = [data.aws_security_group.sg.id]
 
    tags = {
-      Name = "var.component"
+      Name = var.component
    }
 }
 
 resource "aws_route53_record" "record" {
   zone_id = local.zone_id
-  name    = "frontend.${var.zone_id}"
+  name    = "${var.component}.${var.zone_id}"
   type    = "A"
   ttl     = 30
-  records = [ aws_instance.frontend.private_ip ]
+  records = [ aws_instance.instance.private_ip ]
 }
 
 resource "null_resource" "ansible" {
-  depends_on = [aws_route53_record.frontend]
+  depends_on = [aws_route53_record.record]
    provisioner "local-exec" {
       command = <<EOF
 cd /root/Infra-Ansible1
 git pull
 sleep 60
-ansible-playbook -i ${aws_instance.frontend.private_ip}, -e ansible_user=centos -e ansible_password=DevOps321 main.yml -e role_name=frontend
+ansible-playbook -i ${aws_instance.instance.private_ip}, -e ansible_user=centos -e ansible_password=DevOps321 main.yml -e role_name=${var.component}
 EOF
 }
 }
